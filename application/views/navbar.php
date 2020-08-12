fbd64d26ebcce9794451c3ac3bd54d83adf1e4c9
geryakbar <geryakbarfauzi@gmail.com>
2020-08-12 17:35:05 +0700

    Website Portofolio Dengan Codeigniter

--- application/views/navbar.php
+++ application/views/navbar.php
@@ -1,11 +1,19 @@
-<nav class="navbar navbar-dark navbar-expand-lg fixed-top bg-white portfolio-navbar gradient" style="background-color: #2980b9;">
-	<div class="container"><a class="navbar-brand logo" href="#">Portofolio - Gery</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navbarNav"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+<nav class="navbar navbar-dark navbar-expand-lg fixed-top bg-white portfolio-navbar gradient"
+	 style="background-color: #2980b9;">
+	<div class="container"><a class="navbar-brand logo" href="<?php echo site_url('/home/index/') ?>">Portofolio -
+			Gery</a>
+		<button data-toggle="collapse" class="navbar-toggler" data-target="#navbarNav"><span class="sr-only">Toggle navigation</span><span
+					class="navbar-toggler-icon"></span></button>
 		<div class="collapse navbar-collapse"
 			 id="navbarNav">
 			<ul class="nav navbar-nav ml-auto">
-				<li class="nav-item" role="presentation"><a class="nav-link active" href="<?php echo site_url('/home/index/')?>">Home</a></li>
-				<li class="nav-item" role="presentation"><a class="nav-link" href="<?php echo site_url('/project/index/')?>">Projects</a></li>
-				<li class="nav-item" role="presentation"><a class="nav-link" href="<?php echo site_url('/cv/index/')?>">CV</a></li>
+				<li class="nav-item" role="presentation"><a class="nav-link active"
+															href="<?php echo site_url('/home/index/') ?>">Home</a></li>
+				<li class="nav-item" role="presentation"><a class="nav-link"
+															href="<?php echo site_url('/project/index/') ?>">Projects</a>
+				</li>
+				<li class="nav-item" role="presentation"><a class="nav-link"
+															href="<?php echo site_url('/cv/index/') ?>">CV</a></li>
 			</ul>
 		</div>
 	</div>
